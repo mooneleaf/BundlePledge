2afedd86816971cb92461f028dfde4d88ad2b6a4
John Dean <john.holmes.dean@gmail.com>
2020-06-21 12:50:08 -0500

    Linode manifests

--- services/catarse/config/initializers/assets.rb
+++ services/catarse/config/initializers/assets.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
+Rails.application.config.assets.precompile += %w( api/catarse.js )
 Rails.application.config.assets.precompile += %w[catarse_bootstrap/fonts.css catarse_bootstrap/catarse.css jquery.js jquery-ui.js analytics.js redactor.css redactor.js jquery/dist/jquery.js jquery-ui/jquery-ui.js]
