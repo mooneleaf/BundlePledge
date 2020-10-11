fbc88374b4f62000fa5de7ea695fab465aaa63fc
John Dean <john.holmes.dean@gmail.com>
2020-10-11 17:14:42 -0500

    deploy fixes

--- scripts/linode_test_bootstrap.sh
+++ scripts/linode_test_bootstrap.sh
@@ -19,18 +19,18 @@ yarn build
 # Copy the catarse.js dist to RoR Catarase apps assets
 cp dist/* ../catarse/app/assets/javascripts/api/
 cd ../..
-skaffold run -p linode-test-db-init
+skaffold run -p linode-test-db-init --status-check=false
 kubectl -n catarse rollout status -w deploy/service-core-db
 kubectl -n catarse rollout status -w deploy/catarse-db
-skaffold run -p linode-test-migrations
+skaffold run -p linode-test-migrations --status-check=false
 kubectl -n catarse wait --for=condition=complete job/migrations --timeout=60s
-skaffold run -p linode-test-prime
+skaffold run -p linode-test-prime --status-check=false
 kubectl -n catarse wait --for=condition=complete job/db-prime --timeout=60s
-skaffold run -p linode-test-setup
+skaffold run -p linode-test-setup --status-check=false
 kubectl -n catarse wait --for=condition=complete job/fdw-settings --timeout=60s
 kubectl -n catarse wait --for=condition=complete job/demo-settings --timeout=60s
 kubectl -n catarse delete job/catarse-migrations
-skaffold run -p linode-test-migrations
+skaffold run -p linode-test-migrations --status-check=false
 kubectl -n catarse wait --for=condition=complete job/catarse-migrations --timeout=60s
 skaffold run -p linode-test-db
 skaffold run -p linode-test-run
